aa9bdfc64bbeb14a20dc4600be244f3baabe8785
Vineel Kakara <vineelk@zapsight.com>
2026-04-12 17:45:35 +0530

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -6,23 +6,27 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => {
   ctx.reply(`👋 Hey ${ctx.from.first_name}! Ready to log a sales conversation?`, {
     reply_markup: {
-      keyboard: [[{
+      inline_keyboard: [[{
         text: '📋 Open Sales Logger',
         web_app: { url: process.env.FRONTEND_URL }
-      }]],
-      resize_keyboard: true,
-      persistent: true
+      }]]
     }
   });
 });
 
 bot.help((ctx) => {
-  ctx.reply('Tap the 📋 Open Sales Logger button below to log a conversation.');
+  ctx.reply('Tap the button below to log a conversation.', {
+    reply_markup: {
+      inline_keyboard: [[{
+        text: '📋 Open Sales Logger',
+        web_app: { url: process.env.FRONTEND_URL }
+      }]]
+    }
+  });
 });
 
 bot.launch();
 console.log('Bot is running...');
 
-// Graceful shutdown
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
